c92e88714efb62f212b18738b0346e7001e2eda3
motatoes <moe.habib9@gmail.com>
2020-11-12 17:43:52 +0000

    create test script

---
+++ Formula/dg2.rb
@@ -0,0 +1,17 @@
+class Dg < Formula
+    desc "Digger CLI"
+    homepage "https://digger.dev"
+  
+  
+    url "https://digger-releases.s3-eu-west-1.amazonaws.com/dg-darwin-homebrew-0.1.2.zip", :using => :curl
+    
+    bottle :unneeded
+    
+    def install
+      #create_wrapper
+      bin.install_symlink "../dg"
+      prefix.install Dir["dg/.Python"]
+      prefix.install Dir["dg/*"]
+    end
+  
+  end
